c81e3e1c8dc10364dd17510c6c52bb23a921621e
Natalia <nflores.teoria@gmail.com>
2020-08-23 16:29:03 -0400

    pide opción del 1 al 6 y manda a la primera función que falta terminar

---
+++ script.js
@@ -0,0 +1,31 @@
+var opcion = prompt('Hola, elige un número entre el 1 y el 6');
+
+// console.log(opcion);
+
+if (opcion == 1) {
+    opcion1();
+};
+
+// if (opcion == 2) {
+//     2.- preguntar al usuario ¿De qué color es el caballo blanco de Napoleón?. Si responde algo distinto de blanco, debe volver a hacer la misma pregunta. Si responde blanco está ok.
+// };
+
+// if (opcion == 3) {
+//     3.- pedir promedio de notas de Matemáticas, Física y Ciencias. Indicar en pantalla el promedio de notas (alert). El promedio de notas debe estar redondeado a un número entero.
+// };
+
+// if (opcion == 4) {
+//     4.- Pedir el nombre de tres frutas, c/u de manera individual. Una vez ingresadas todas deben imprimirse todas por consola, a excepción de manzana.
+// };
+
+// if (opcion == 5) {
+//     5.- Pedir nombre, determinar la cantidad de vocales y consonantes y mostrarlos en pantalla con alert.
+// };
+
+function opcion1 (num) {
+    let number = prompt('Por favor ingresa un número entre el 1 y el 100');
+    if (number)
+    //1.- ejecutar función que obtenga un número del 1 al 100 del usuario y devuelva en la consola cada uno de los números del 1 al número elegido.
+};
+
+
