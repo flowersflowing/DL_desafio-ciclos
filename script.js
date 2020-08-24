c8c00b67f64b99b9bfe80577cdb092730428cd5f
Natalia <nflores.teoria@gmail.com>
2020-08-24 17:58:25 -0400

    Todas las opciones funcionando, falta redondear promedio del 3 y excluir manzana en el 4

--- script.js
+++ script.js
@@ -36,6 +36,7 @@ if (opcion == 6) {
     document.write('Hola, ingresaste el 6 y no había instrucción para este caso.');
 };
 
+
 //FUNCIONES
 
 //Función para pedir un número del 1 al 100 y que devuelva en consola todos los números del 1 al seleccionado
@@ -86,24 +87,29 @@ function opcion4 () {
     let roja = prompt('Ingresa el nombre de una fruta roja');
     let verde = prompt('Ingresa el nombre de la fruta verde');
     let amarilla = prompt('Ingresa el nombre de una fruta amarilla');
-
+    
+    const patron = /manzana/gim;
     let arrFrutas = [roja, verde, amarilla];
 
     for (const fruta of arrFrutas) {
         console.log(fruta);        
-    } //que no imprima la manzana
+    }
 
     /* let arrFrutas = {
         fruta1: roja, 
         fruta2: verde, 
-        fruta3: amarilla,
+        fruta3: amarilla,    
     };
     console.log(arrFrutas.fruta1, arrFrutas.fruta2, arrFrutas.fruta3);*/
 };
 
-//5.- Pedir nombre, determinar la cantidad de vocales y consonantes y mostrarlos en pantalla con alert.
+//5.- Pedir nombre, determinar la cantidad de vocales y consonantes y mostrar la cantidad de cada uno en pantalla con alert.
 
 function opcion5 () {
-    prompt('Hola, por favor ingresa tu nombre:');
+    let name = prompt('Hola, por favor ingresa tu nombre:');
     
+    let vowels = name.match(/[aeiouáéíóú]/gim);
+    let consons = name.match(/[^aeiouáéíóú]/gim);
+    
+    alert('Tu nombre tiene ' + vowels.length + ' (' + vowels + ') vocales y tiene ' + consons.length + ' (' + consons + ') consonantes.');
 };
